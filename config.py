f334373e024f023a8c97b7590d4e70205dc1e55e
yurastrus <yurastrus@gmail.com>
2026-02-17 11:13:30 +0200

    config update

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ import os
 from datetime import timedelta
 from dotenv import load_dotenv
 
-basedir = os.path.abspath(os.path.dirname(__file__))
+basedir = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
 
 load_dotenv(os.path.join(basedir, '.env'))
 
@@ -23,6 +23,21 @@ class Config:
     GEODATA_DATABASE_URI = os.environ.get('GEODATA_DATABASE_URI') 
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # === НАЛАШТУВАННЯ ДЛЯ МОДУЛЯ CAMERA TRAPS ===
+    CAMERA_TRAP_CONFIG = {
+        'MAX_FILE_SIZE': 10 * 1024 * 1024,  # 10MB
+        'THUMBNAIL_SIZE': (800, 800),
+        'SERIES_TIME_WINDOW': 60,  # секунди для групування в серію
+        'ALLOWED_EXTENSIONS': {'jpg', 'jpeg'},
+        'MIN_IDENTIFICATIONS': 2,  # мінімум ідентифікацій для консенсусу
+        'CLEANUP_DAYS': 0,  # дні після яких видаляти фото
+        'STALE_BATCH_HOURS': 0
+    }
+
+    PAM_MAX_UPLOAD_SIZE = 1000 * 1024 * 1024  # 500 MB для ZIP архівів
+    PAM_ALLOWED_AUDIO_EXTENSIONS = {'.wav'}
+    # ліміт кількості виділів які будуть показані на карті переглядача даних лісовпорядкування.
     
     # Налаштування мов
     LANGUAGES = {'en': 'English', 'uk': 'Українська'}
@@ -44,10 +59,21 @@ class Config:
 class DevelopmentConfig(Config):
     DEBUG = True
     GEOSERVER_URL = os.environ.get('GEOSERVER_URL') or 'http://91.99.138.240:8080/geoserver'
+    CAMERA_TRAP_CONFIG = Config.CAMERA_TRAP_CONFIG.copy()
+    CAMERA_TRAP_CONFIG.update({
+        'UPLOAD_PATH': os.path.join(basedir, 'camera_trap_data')
+    })
+    PAM_UPLOAD_PATH = os.path.join(basedir, 'pam_data_import/segments')
 
 class ProductionConfig(Config):
     DEBUG = False
     GEOSERVER_URL = os.environ.get('GEOSERVER_URL') or '/geoserver'
+    CAMERA_TRAP_CONFIG = Config.CAMERA_TRAP_CONFIG.copy()
+    CAMERA_TRAP_CONFIG.update({
+        'UPLOAD_PATH': os.environ.get('CAMERA_TRAP_UPLOAD_PATH')
+    })
+    PAM_UPLOAD_PATH = os.environ.get('PAM_UPLOAD_PATH')
+
 
 class TestingConfig(Config):
     """Конфігурація для тестування"""
