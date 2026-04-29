ad7ea188079cac776b02cdf789aff0c72aafa454
yurastrus <yurastrus@gmail.com>
2026-04-29 13:45:08 +0300

    fix: update.sh uses reset --hard instead of pull to avoid divergent branch errors

--- update.sh
+++ update.sh
@@ -8,10 +8,11 @@ set -e  # зупинитись при будь-якій помилці
 echo "--- Починаю оновлення biomon ---"
 
 # 1. Оновлюємо головний проект
-git pull origin master
+git fetch origin
+git reset --hard origin/master
 
 # 2. Оновлюємо всі підмодулі (ПАМ, Фотопастки, SDM)
-git submodule update --init --recursive
+git submodule update --init --recursive --force
 
 # 3. Встановлюємо/оновлюємо Python-залежності (якщо змінились)
 venv/bin/pip install -q -r requirements.txt
